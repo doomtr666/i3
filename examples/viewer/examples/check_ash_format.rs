a2ddcc87afbf7e57389263c7b03b86959d38170b
Christophe Delacourt <christophe.delacourt.perso@gmail.com>
2026-03-17 08:27:17 +0100

    cleanup

--- examples/viewer/examples/check_ash_format.rs
+++
@@ -1,7 +0,0 @@
-use ash::vk;
-
-fn main() {
-    println!("B10G11R11_UFLOAT_PACK32: {:?}", vk::Format::B10G11R11_UFLOAT_PACK32);
-    // Let's see if R11G11B10 exists
-    // println!("R11G11B10_UFLOAT_PACK32: {:?}", vk::Format::R11G11B10_UFLOAT_PACK32);
-}
